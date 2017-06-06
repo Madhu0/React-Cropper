f014c9f2b41aa52a2a105237e05c3d1ebac45a9a
Madhu0 <kmadhusudhanreddy3@gmail.com>
2017-06-06 21:56:44 +0530

    added cropbox and it is movable

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ export default class ImageCropper extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      pos: {
+        x1: 0,
+        y1: 0,
+        x2: 0,
+        y2: 0,
+      },
+      showCropBox: false,
+    };
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -53,14 +61,60 @@ export default class ImageCropper extends React.Component {
   }
 
   handleDrag = (e, id) => {
-    console.log("drag", e, id);
-    e.dataTransfer.setData("text", e.target.id);  
+    // console.log("drag", e, id);
+    // e.dataTransfer.setData("text", e.target.id);
+    if (id === 'left') {
+      this.setState({
+        pos : Object.assign({}, this.state.pos, {
+          x1: e.pageX,
+        })
+      });
+    } else if (id === 'right') {
+      this.setState({
+        pos : Object.assign({}, this.state.pos, {
+          x2: e.pageX,
+        })
+      });
+    } else if (id === 'top') {
+      this.setState({
+        pos : Object.assign({}, this.state.pos, {
+          y1: e.pageY,
+        })
+      });
+    } else {
+      this.setState({
+        pos : Object.assign({}, this.state.pos, {
+          y2: e.pageY,
+        })
+      });
+    }
   }
 
   handleDrop = (e) => {
     console.log("drop", e);
     const data = e.dataTransfer.getData('text');
     console.log("data", data);
+  }
+
+  handleImageDragStart = (e) => {
+    this.startingCoords = {
+      x: e.pageX,
+      y: e.pageY,
+    };
+    console.log("in drag start");
+  }
+
+  handleImageDragEnd = (e) => {
+    this.setState({
+      pos: {
+        x1: this.startingCoords.x,
+        y1: this.startingCoords.y,
+        x2: e.pageX,
+        y2: e.pageY,
+      },
+      showCropBox: true,
+    });
+    console.log("in drag end");
   }
 
   render() {
@@ -76,15 +130,24 @@ export default class ImageCropper extends React.Component {
       this.canvas.height = image.height;
       this.ctx.drawImage(image, 0, 0, image.width, image.height);
     }
+    console.log("state", this.state)
     image.src = src;
+    let cropBoxHeight = this.state.pos.y1 - this.state.pos.y2;
+    if (cropBoxHeight < 0) {
+      cropBoxHeight *= -1;
+    }
+    let cropBoxWidth = this.state.pos.x1 - this.state.pos.x2;
+    if (cropBoxWidth < 0) {
+      cropBoxWidth *= -1;
+    }
     return (<div className={containerClassName}> 
-      <div id="grooot" onDrop={this.handleDrop} onDragOver={(e) => { e.preventDefault(); }} />
-      <div className="cropper" draggable={true} onDrag={ (e) => { this.handleDrag(e, 0) } } style={{ left:this.state.pos.left, right:this.state.pos.right }}>
-        <span className="left" id="left" onDrag={ (e) => { this.handleDrag(e, 0) } } />
-        <span className="right" id="right" onDrag={ (e) => { this.handleDrag(e, 1) } }/>
-        <span className="top" id="top" onDrag={ (e) => { this.handleDrag(e, 2) } } />
-        <span className="bottom" id="bottom" onDrag={ (e) => { this.handleDrag(e, 3) } } />
-      </div>
+      <div id="grooot" draggable={true} onDragStart={(e) => { this.handleImageDragStart(e); }} onDragEnd={(e) => { this.handleImageDragEnd(e) }} />
+      {this.state.showCropBox && <div className="cropper" draggable={true} >
+        <span className="left" id="left" draggable={true} onDragEnd={ (e) => { this.handleDrag(e, 'left') } } style={{ left:this.state.pos.x1, top:this.state.pos.y1, height: cropBoxHeight }} />
+        <span className="right" id="right" draggable={true} onDragEnd={ (e) => { this.handleDrag(e, 'right') } } style={{ left:this.state.pos.x2, top:this.state.pos.y1, height: cropBoxHeight }} />
+        <span className="top" id="top" draggable={true} onDragEnd={ (e) => { this.handleDrag(e, 'top') } } style={{ left:this.state.pos.x1, top:this.state.pos.y1, width: cropBoxWidth }} />
+        <span className="bottom" id="bottom" draggable={true} onDragEnd={ (e) => { this.handleDrag(e, 'bottom') } } style={{ left:this.state.pos.x1, top:this.state.pos.y2, width: cropBoxWidth }} />
+      </div>}
       <button onClick={this.handleCrop} >Crop</button>
       { this.state.imageSrc && <img src={this.state.imageSrc} /> }
     </div>)
